5f8c3d1b85cc81581e20d02b079f61f85ee67d60
Dinesh <dk159219@gmail.com>
2022-12-13 22:24:13 +0530

    tryy

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,20 +5,20 @@ public class Main {
     class Product{
         public int product(int x, int y)
         {
-
+return x*y;
         }
         public int product(int x, int y, int z) {
-
+return x*y*z;
         }
         public double product(double x, double y) {
-
+            return x*y;
         }
     }
 public void main(String [] args)
 {
     Product p = new Product();
-    int x,y,z;
-    double f,e;
+    int x=1,y=1,z=1;
+    double f=1,e=1;
    p.product(x,y);
    p.product(x,y,z);
    p.product(f,e);
